67000cfc01f3af26a0ed9be0cc40c59c118a2b1f
Zihang Chen <chsc4698@gmail.com>
2016-05-23 09:11:39 -0400

    rewrite test config

--- scripting/rrr.lua
+++ scripting/rrr.lua
@@ -1,38 +1,7 @@
-local rrr = {}
+rrr = {}
 
-function rrr.rgb(r, g, b)
-  return { r=r
-         , g=g
-         , b=b
-         , type="rgb" }
-end
-
-function test_type(x, t)
-  if x.type ~= t then
-    return false
-  end
-
-  return true
-end
-
-function rrr.material(diff, spec, spec_r, ispec, glossy, refr, ni)
-  local legal = (test_type(diff, 'rgb')
-                 or test_type(spec, 'rgb')
-                 or test_type(ispec, 'rgb')
-                 or test_type(refr, 'rgb')
-                 or (not test_type(spec_r, 'rgb'))
-                 or (not test_type(glossy, 'rgb'))
-                 or (not test_type(ni, 'rgb')))
-  if not legal then
-    return false
-  end
-
-  return { diff=diff
-         , spec=sepc, spec_r=spec_r
-         , ispec=ispec
-         , glossy=glossy
-         , refr=refr, ni=ni }
-end
-
-function rrr.triangle(p1, p2, p3)
+function rrr.v3(x, y, z)
+    return { x = x
+        , y = y
+        , z = z }
 end
--- scripting/scene.lua
+++ scripting/scene.lua
@@ -1,16 +1,30 @@
+function v3(x, y, z)
+    return { x = x
+           , y = y
+           , z = z }
+end
 
+cam = RLCamera.new(v3(3, 3, 3),
+                   v3(-0.7, -0.7, -0.7),
+                   35.0, 35.0, 25.0, 400, 300)
 
 surf_buf = RLSurfaceBuf.new()
+light_buf = RLLightBuf.new()
+mtr_buf = RLMaterialBuf.new()
 
-scene = RLScene.new()
+scene = RLScene.new(surf_buf, light_buf, mtr_buf, cam)
 
 
-triangles = load_wavefront_obj("bunny.obj")
-materials = {rrr.material(rrr.rgb(0.0, 0.0, 0.0),
-                          rrr.rgb(0.0, 0.0, 0.0),
-                          0.0,
-                          rrr.rgb(0.0, 0.0, 0.0),
-                          10,
-                          rrr.rgb(0.0, 0.0, 0.0),
-                          0.5)}
+--
+--scene:install_surf_buf(surf_buf)
+
+--
+--triangles = load_wavefront_obj("bunny.obj")
+--materials = {rrr.material(rrr.rgb(0.0, 0.0, 0.0),
+--                          rrr.rgb(0.0, 0.0, 0.0),
+--                          0.0,
+--                          rrr.rgb(0.0, 0.0, 0.0),
+--                          10,
+--                          rrr.rgb(0.0, 0.0, 0.0),
+--                          0.5)}
 
