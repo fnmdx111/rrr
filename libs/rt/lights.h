8ef5a6376f5336249d2dd04a51046ec2d744af17
Zihang Chen <chsc4698@gmail.com>
2016-05-23 10:08:45 -0400

    add lights def

--- libs/rt/lights.h
+++ libs/rt/lights.h
@@ -5,9 +5,42 @@
 #ifndef RRR_LIGHTS_H
 #define RRR_LIGHTS_H
 
+#include "../math/vec.h"
+
+struct r_ambient_light
+{
+  v31v color;
+};
+
+struct r_point_light
+{
+  p31v pos;
+  v31v color;
+};
+
+struct r_direction_light
+{
+  v31v dir;
+  v31v color;
+};
+
+struct r_area_light
+{
+};
+
+enum r_light_type
+{
+  AMBIENT, POINT, DIRECTION, AREA
+};
 struct light
 {
-
+  enum r_light_type type;
+  union {
+    struct r_ambient_light ambient;
+    struct r_point_light point;
+    struct r_direction_light direction;
+    struct r_area_light area;
+  };
 };
 
 typedef struct light r_light;
